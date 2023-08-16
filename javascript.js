e987fa60a58c7335d1cbcefcdd258d3bc5095181
Franek Sornat <62433571+Kaptvr@users.noreply.github.com>
2023-08-16 13:18:00 +0200

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function getComputerChoice ()
 {
-  choice = Math.floor(Math.random() * 3);
+  let choice = Math.floor(Math.random() * 3);
   if (choice === 0)
   {
     return "Rock";
@@ -15,4 +15,28 @@ function getComputerChoice ()
   }
 }
 
-console.log(getComputerChoice());
+function playRound (computerSelection, playerSelection)
+{
+  playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+
+  if (computerSelection === playerSelection)
+  {
+    return "It's a Tie"
+  }
+  else if (computerSelection === "Rock" && playerSelection === "Scissors" ||
+          computerSelection === "Paper" && playerSelection === "Rock" ||
+          computerSelection === "Scissors" && playerSelection === "Paper")
+          {
+            return `You Lose! ${computerSelection} beats ${playerSelection}`
+          }
+  else
+  {
+    return `You Win! ${playerSelection} beats ${computerSelection}`
+  }
+}
+
+const computerChoice = getComputerChoice();
+const playerChoice = "Scissors";
+
+console.log(playRound(computerChoice, playerChoice));
