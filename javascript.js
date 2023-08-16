51fc144f022e0c644662db9780f55eeff5452b40
Franek Sornat <62433571+Kaptvr@users.noreply.github.com>
2023-08-16 13:41:36 +0200

    Finish game in javascript.js

--- javascript.js
+++ javascript.js
@@ -15,6 +15,9 @@ function getComputerChoice ()
   }
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound (computerSelection, playerSelection)
 {
   playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
@@ -22,21 +25,61 @@ function playRound (computerSelection, playerSelection)
 
   if (computerSelection === playerSelection)
   {
-    return "It's a Tie"
+    return "It's a Tie";
   }
   else if (computerSelection === "Rock" && playerSelection === "Scissors" ||
           computerSelection === "Paper" && playerSelection === "Rock" ||
           computerSelection === "Scissors" && playerSelection === "Paper")
-          {
-            return `You Lose! ${computerSelection} beats ${playerSelection}`
-          }
+  {
+    computerScore++;
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+  else if (playerSelection === "Rock" && computerSelection === "Scissors" ||
+          playerSelection === "Paper" && computerSelection === "Rock" ||
+          playerSelection === "Scissors" && computerSelection === "Paper")
+  {
+    playerScore++;
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  }
   else
   {
-    return `You Win! ${playerSelection} beats ${computerSelection}`
+    return "Invalid choice"
   }
 }
 
-const computerChoice = getComputerChoice();
-const playerChoice = "Scissors";
+function pickWinner (playerScore, computerScore)
+{
+  if (playerScore > computerScore)
+  {
+    return "Player wins!";
+  }
+  else if (computerScore > playerScore)
+  {
+    return "Computer wins!"
+  }
+  else
+  {
+    return "Tie!"
+  }
+}
 
-console.log(playRound(computerChoice, playerChoice));
+function game ()
+{
+  for (let i = 0; i < 5; i++)
+  {
+    const computerChoice = getComputerChoice();
+    const playerChoice = prompt(`(Round ${i+1} out of 5) Choose Rock Paper or Scissors`);
+
+    let verdict = playRound(computerChoice, playerChoice);
+    console.log(verdict);
+
+    if (verdict === "Invalid choice")
+    {
+      i--;
+    }
+  }
+
+  console.log(pickWinner(playerScore, computerScore));
+}
+
+game ();
